4e28a3497a7d71c04b4aa35a3146b20647eed171
mrkwon <erased11@gmail.com>
2019-11-02 22:32:53 +0900

    feat: Icon Button

---
+++ components/IconButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <a class="wrapper" :href="href">
+    <v-icon class="icon-btn" :color="color" large>{{ icon }}</v-icon>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    href: {
+      type: String
+    },
+    icon: {
+      type: String
+    },
+    color: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style>
+  .wrapper {
+    text-decoration: none;
+    margin: 8px;
+  }
+</style>
