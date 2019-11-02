5cdc2608121fa1aaf8f23a54e60e8f8dc324a13b
mrkwon <erased11@gmail.com>
2019-11-03 03:52:47 +0900

    chore: remove import

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,8 +12,7 @@ module.exports = {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Do+Hyeon&display=swap'},
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Noto+Sans+KR&display=swap'},
-      { rel: 'stylesheet', href: 'https://unpkg.com/fullpage.js/dist/fullpage.extensions.min.css'}
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Noto+Sans+KR&display=swap'}
     ],
     script: [
     ]
